77b106e558bb8de52da58b5fdb3ba5ee53aefef3
asingh20-tech <asingh20@oswego.edu>
2026-03-24 20:15:55 -0400

    first commit

---
+++ src/main/scala/example/Hello.scala
@@ -0,0 +1,17 @@
+package example
+
+
+object valuesandtypes {
+  val jmd : Int = 42
+
+  val int = 43+ 47
+
+}
+
+// object Hello extends Greeting with App {
+//   println(greeting)
+// }
+
+// trait Greeting {
+//   lazy val greeting: String = "hello"
+// }
